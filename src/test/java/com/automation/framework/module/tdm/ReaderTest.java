528b89d226bd627903018089b1f467f14be07617
Jeet <suryajit7@gmail.com>
2021-10-25 09:41:51 +0530

    Commit57: Added few file reader tests

--- src/test/java/com/automation/framework/module/tdm/ReaderTest.java
+++ src/test/java/com/automation/framework/module/tdm/ReaderTest.java
@@ -5,13 +5,30 @@ import com.automation.framework.data.entity.app.ems.Department;
 import com.automation.framework.data.entity.app.ems.Employee;
 import com.automation.framework.util.FileReader;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Properties;
 
 
 public class ReaderTest extends BaseTestNGTest {
 
     @Autowired
     private FileReader fileReader;
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @Value("${user.dir}/datasets-file.csv")
+    private Resource csvResource;
+
+    @Value("https://www.w3.org/TR/PNG/iso_8859-1.txt")
+    private Resource textFile;
 
     @Test(priority = 0)
     public void verifyFileReaderUtility() throws Exception {
@@ -26,8 +43,21 @@ public class ReaderTest extends BaseTestNGTest {
 
         fileReader.readExcelFile("DepartmentTestData.xlsx", Department.class, "Sheet1")
                 .forEach(System.out::println);
+    }
 
+    @Test(priority = 1)
+    public void verifyValueInjection() throws IOException {
+        Files.readAllLines(csvResource.getFile().toPath())
+                .forEach(System.out::println);
 
+        System.out.printf(new String(textFile.getInputStream().readAllBytes()));
+    }
+
+    @Test(priority = 2)
+    public void testPropertiesFile() throws IOException {
+
+        Properties property = PropertiesLoaderUtils.loadProperties(resourceLoader.getResource("property-test.properties"));
+        System.out.printf(String.valueOf(property));
     }
 
 
