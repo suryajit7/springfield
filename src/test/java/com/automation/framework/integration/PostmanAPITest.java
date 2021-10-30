593b2766c0648219c0c074ddde66a5e48a4deb49
Suraj <suryajit7@gmail.com>
2021-10-30 18:25:50 +0530

    Commit61: Added simple API tests

--- src/test/java/com/automation/framework/integration/PostmanAPITest.java
+++ src/test/java/com/automation/framework/integration/PostmanAPITest.java
@@ -4,84 +4,145 @@ import com.automation.framework.AutomationSuiteApplicationTests;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 
+import java.util.Set;
+
+import static io.restassured.RestAssured.config;
 import static io.restassured.RestAssured.given;
+import static io.restassured.config.LogConfig.logConfig;
+import static org.apache.hc.core5.http.HttpStatus.SC_SUCCESS;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.*;
 
 public class PostmanAPITest extends AutomationSuiteApplicationTests {
 
+    @Value("${app.postman.url}")
+    private String postmanUrl;
+
+    @Value("${app.api.key}")
+    private String apiKey;
 
     @Test
-    public void simpleRestTest(){
+    public void verifySimpleRestTest(){
 
-                given()
-                .baseUri("https://api.postman.com")
-                .header("X-Api-Key", "PMAK-6176c15477601e003b81a9cf-9e15801f58985a5cd0b28187bc0bb39e33")
-        .when()
+        given()
+                .baseUri(postmanUrl)
+                .header("X-Api-Key", apiKey)
+                .when()
                 .get("/workspaces")
-        .then()
-                .statusCode(200);
+                .then()
+                .statusCode(SC_SUCCESS);
     }
 
     @Test
     public void verifyGetStatusCode(){
 
         given()
-                .baseUri("https://api.postman.com")
-                .header("X-Api-Key", "PMAK-6176c15477601e003b81a9cf-9e15801f58985a5cd0b28187bc0bb39e33")
-        .when()
+                .baseUri(postmanUrl)
+                .header("X-Api-Key", apiKey)
+                .when()
                 .get("/workspaces")
-        .then()
+                .then()
                 .log().all()
-                .assertThat().statusCode(200);
+                .assertThat().statusCode(SC_SUCCESS);
     }
-
 
     @Test
     public void verifyGetResponseCode(){
         given()
-                .baseUri("https://api.postman.com")
-                .header("X-Api-Key", "PMAK-6176c15477601e003b81a9cf-9e15801f58985a5cd0b28187bc0bb39e33")
-        .when()
+                .baseUri(postmanUrl)
+                .header("X-Api-Key", apiKey)
+                .when()
                 .get("/workspaces")
-        .then()
+                .then()
                 .log().all()
-                .assertThat().statusCode(200)
-                .body("workspaces.name", hasItems("Team Workspace, MyWorkspace, My Workspace, Test", "Test"),
+                .assertThat().statusCode(SC_SUCCESS)
+                .body("workspaces.name", is(not(emptyArray())),
+                        "workspaces.name", contains("Team Workspace", "MyWorkspace", "My Workspace", "Test", "Test"),
+                        "workspaces.name", containsInAnyOrder("MyWorkspace", "Team Workspace", "My Workspace", "Test", "Test"),
+                        "workspaces.name", hasItem("MyWorkspace"),
                         "workspaces[0].name", equalTo("Team Workspace"),
-                        "workspaces.size()", equalTo(5));
+                        "workspaces.name", hasSize(5),
+                        "workspaces[0]", hasKey("type"),
+                        "workspaces[0]", hasValue("Team Workspace"),
+                        "workspaces[0]", hasEntry("name", "Team Workspace"));
+
     }
 
     @Test
     public void verifyExtractResponse(){
         //Example 1
         Response response = given()
-                .baseUri("https://api.postman.com")
-                .header("X-Api-Key", "PMAK-6176c15477601e003b81a9cf-9e15801f58985a5cd0b28187bc0bb39e33")
+                .baseUri(postmanUrl)
+                .header("X-Api-Key", apiKey)
                 .when()
                 .get("/workspaces")
                 .then()
-                .statusCode(200)
+                .statusCode(SC_SUCCESS)
                 .extract().response();
+
         String jsonPath = new JsonPath(response.asString()).getString("workspaces[0].name");
         System.out.println("workspace name = " + jsonPath);
 
 
         //Example 2
         String workspaceName = given()
-                .baseUri("https://api.postman.com")
-                .header("X-Api-Key", "PMAK-6176c15477601e003b81a9cf-9e15801f58985a5cd0b28187bc0bb39e33")
+                .baseUri(postmanUrl)
+                .header("X-Api-Key", apiKey)
                 .when()
                 .get("/workspaces")
                 .then()
-                .statusCode(200)
+                .statusCode(SC_SUCCESS)
                 .extract().response().path("workspaces[0].name");
 
         System.out.println("workspace name = " + workspaceName);
 
         assertThat(workspaceName, equalTo("Team Workspace"));
     }
+
+    @Test
+    public void verifyLoggingForRequestAndResponse(){
+
+        given()
+                .baseUri(postmanUrl)
+                .header("X-Api-Key", apiKey)
+                .log().all()
+                .when()
+                .get("/workspaces")
+                .then()
+                .log().all()
+                .statusCode(SC_SUCCESS);
+    }
+
+    @Test
+    public void verifyLoggingIfValidationFails(){
+
+        given()
+                .baseUri(postmanUrl)
+                .header("X-Api-Key", apiKey)
+                .config(config().logConfig(logConfig().enableLoggingOfRequestAndResponseIfValidationFails()))
+                .when()
+                .get("/workspaces")
+                .then()
+                .statusCode(SC_SUCCESS);
+    }
+
+
+    @Test
+    public void verifyLoggingBlackListedHeaders(){
+
+        Set blackListHeaders = Set.of("X-Api-Key", "Accept");
+        given()
+                .baseUri(postmanUrl)
+                .header("X-Api-Key", apiKey)
+                .config(config().logConfig(logConfig().blacklistHeaders(blackListHeaders)))
+                .log().all()
+                .when()
+                .get("/workspaces")
+                .then()
+                .statusCode(SC_SUCCESS);
+    }
+
 
 }
