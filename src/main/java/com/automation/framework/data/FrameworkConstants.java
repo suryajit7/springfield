6de3a61c49e0ffe9f9fbd161819030979d684ef4
Suraj <suryajit7@gmail.com>
2021-11-01 18:35:45 +0530

    Commit73: Parallel test support config by adding junit-platform.properties

--- src/main/java/com/automation/framework/data/FrameworkConstants.java
+++ src/main/java/com/automation/framework/data/FrameworkConstants.java
@@ -7,11 +7,15 @@ public class FrameworkConstants {
 
     public static final String TEST_RESOURCE = "";
 
-
     public static final String HEADER = "header";
+    public static final String MULTI_VALUE_HEADER = "multiValueHeader";
     public static final String X_API_KEY_HEADER = "X-Api-Key";
     public static final String X_MOCK_MATCH_REQUEST_HEADERS = "x-mock-match-request-headers";
-    //public static final String X_API_KEY_HEADER = "X-Api-Key";
+    public static final String X_SRV_TRACE = "x-srv-trace";
+    public static final String X_SRV_SPAN = "x-srv-span";
+    public static final String X_RATELIMIT_LIMIT = "X-RateLimit-Limit";
+    public static final String X_RATELIMIT_REMAINING = "X-RateLimit-Remaining";
+    public static final String X_RATELIMIT_RESET = "X-RateLimit-Reset";
 
 
 
