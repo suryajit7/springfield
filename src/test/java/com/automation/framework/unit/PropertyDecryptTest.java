bf7853368995f9466ec8ee9d8300ee63f79c3232
Jeet <suryajit7@gmail.com>
2021-12-24 01:50:12 +0530

    Commit105: minor refactoring.

--- src/test/java/com/automation/framework/unit/PropertyDecryptTest.java
+++ src/test/java/com/automation/framework/unit/PropertyDecryptTest.java
@@ -1,18 +1,19 @@
 package com.automation.framework.unit;
 
 import com.automation.framework.AutomationSuiteApplicationTests;
-import com.automation.framework.core.annotation.LazyAutowired;
 import com.automation.framework.service.SpecBuilder;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static com.automation.framework.data.Constants.BEARER;
 
 public class PropertyDecryptTest extends AutomationSuiteApplicationTests {
 
-    @LazyAutowired
+    @Autowired
     public SpecBuilder specBuilder;
+
     @Test
     @Order(1)
     public void verifyGivenFieldIsValidPhoneNumber(){
