bb064511b8b3f3afc30601ba5050b622dae5f09e
Jeet <suryajit7@gmail.com>
2021-12-24 20:58:22 +0530

    Commit112: Minor refactoring

--- src/test/java/com/automation/framework/unit/PropertyDecryptTest.java
+++ src/test/java/com/automation/framework/unit/PropertyDecryptTest.java
@@ -1,7 +1,7 @@
 package com.automation.framework.unit;
 
 import com.automation.framework.AutomationSuiteApplicationTests;
-import com.automation.framework.service.SpecBuilder;
+import com.automation.framework.service.RestResource;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -12,13 +12,13 @@ import static com.automation.framework.data.Constants.BEARER;
 public class PropertyDecryptTest extends AutomationSuiteApplicationTests {
 
     @Autowired
-    public SpecBuilder specBuilder;
+    public RestResource resource;
 
     @Test
     @Order(1)
     public void verifyGivenFieldIsValidPhoneNumber(){
 
-        Assertions.assertThat(specBuilder.decryptService.getSpotifyAccessToken(false))
+        Assertions.assertThat(resource.decryptService.getSpotifyAccessToken(false))
                 .isNotBlank()
                 .startsWith(BEARER)
                 .contains(BEARER);
