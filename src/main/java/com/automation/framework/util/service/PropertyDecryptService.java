63a750d61c3ad32df516ee32e0080e21df614a89
Jeet <suryajit7@gmail.com>
2021-12-24 21:07:00 +0530

    Commit114: Minor refactoring

--- src/main/java/com/automation/framework/util/service/PropertyDecryptService.java
+++ src/main/java/com/automation/framework/util/service/PropertyDecryptService.java
@@ -6,11 +6,32 @@ import org.springframework.beans.factory.annotation.Value;
 @LazyService
 public class PropertyDecryptService {
 
+    @Value("${app.spotify.client-Id")
+    private String spotifyClientId;
+
+    @Value("${app.spotify.client-secret}")
+    private String spotifyClientSecret;
+
     @Value("${app.spotify.key}")
     private String spotifyAccessToken;
 
+    @Value("${app.spotify.refresh-token}")
+    private String spotifyRefreshToken;
+
     @Value("${app.spotify.expired-key}")
     private String spotifyExpiredAccessToken;
+
+    public String getSpotifyClientId() {
+        return spotifyClientId;
+    }
+
+    public String getSpotifyClientSecret() {
+        return spotifyClientSecret;
+    }
+
+    public String getSpotifyRefreshToken() {
+        return spotifyRefreshToken;
+    }
 
     public String getSpotifyAccessToken(Boolean expiredToken) {
         return expiredToken ? spotifyExpiredAccessToken : spotifyAccessToken;
