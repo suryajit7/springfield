eee9b21ed3796ae8b8b301cc4cc5a761a619a30e
suryajit7 <suryajit7@gmail.com>
2022-05-08 00:45:49 +0530

    Commit199: adding travel domain and minor refactoring

--- src/test/java/com/automation/framework/BaseTestNGTest.java
+++ src/test/java/com/automation/framework/BaseTestNGTest.java
@@ -7,8 +7,6 @@ import com.automation.framework.util.TestExecutionListener;
 import com.github.javafaker.Faker;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
@@ -16,7 +14,6 @@ import org.testng.annotations.Listeners;
 
 
 @SpringBootTest
-@ContextConfiguration(loader= AnnotationConfigContextLoader.class, classes = SpringfieldApplication.class)
 @Listeners(TestExecutionListener.class)
 public class BaseTestNGTest extends AbstractTestNGSpringContextTests {
 
--- src/test/java/com/automation/framework/TestRunner.java
+++
@@ -1,4 +0,0 @@
-package com.automation.framework;
-
-public class TestRunner {
-}
