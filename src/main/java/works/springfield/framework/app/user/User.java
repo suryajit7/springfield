336268af94b60daed217113d3e3a3131202f4f30
suryajit7 <suryajit7@gmail.com>
2022-08-23 19:25:44 +0530

    SPRINGFIELD-003: Added Dockerfile, logback config, sample User api

---
+++ src/main/java/works/springfield/framework/app/user/User.java
@@ -0,0 +1,11 @@
+package works.springfield.framework.app.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+
+    private String name;
+}
